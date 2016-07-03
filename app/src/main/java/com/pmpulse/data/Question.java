c8e42be01c32077ebcbe99c7987acb83d2f6a8c4
shradhavijay <shradha_vijay@yahoo.in>
2016-07-03 16:34:29 +0530

    Add data type for question

---
+++ app/src/main/java/com/pmpulse/data/Question.java
@@ -0,0 +1,80 @@
+package com.pmpulse.data;
+
+/**
+ * Created by shradha on 3/7/16.
+ */
+public class Question {
+
+    private String question;
+    private String answerA;
+    private String answerB;
+    private String answerC;
+    private String answerD;
+    private String correctAnswer;
+    private boolean marked;
+    private boolean isCorrect;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswerA() {
+        return answerA;
+    }
+
+    public void setAnswerA(String answerA) {
+        this.answerA = answerA;
+    }
+
+    public String getAnswerB() {
+        return answerB;
+    }
+
+    public void setAnswerB(String answerB) {
+        this.answerB = answerB;
+    }
+
+    public String getAnswerC() {
+        return answerC;
+    }
+
+    public void setAnswerC(String answerC) {
+        this.answerC = answerC;
+    }
+
+    public String getAnswerD() {
+        return answerD;
+    }
+
+    public void setAnswerD(String answerD) {
+        this.answerD = answerD;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public boolean isMarked() {
+        return marked;
+    }
+
+    public void setMarked(boolean marked) {
+        this.marked = marked;
+    }
+
+    public boolean isCorrect() {
+        return isCorrect;
+    }
+
+    public void setCorrect(boolean correct) {
+        isCorrect = correct;
+    }
+}
